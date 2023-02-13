1231296662fedc3374885add1c28286c5de58285
ant0d0v <gosu1203@gmail.com>
2023-02-13 17:29:26 +0200

    allure

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -44,9 +44,10 @@ public abstract class BaseTest {
 
     @AfterMethod
     protected void afterMethod(Method method, ITestResult result) {
-        if (!result.isSuccess()&& BaseUtils.isServerRun()) {
+        if (!result.isSuccess()) {
             BaseUtils.captureScreenFile(driver, method.getName(), this.getClass().getName());
         }
+
         BaseUtils.logf("Execution time is %o sec\n\n", (result.getEndMillis() - result.getStartMillis()) / 1000);
         Reporter.log(ReportUtils.getTestStatistics(method, result), true);
 
