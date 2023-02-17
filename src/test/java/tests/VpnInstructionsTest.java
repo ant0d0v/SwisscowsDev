83b693045986db8de5d0c577a367709a5eaea358
ant0d0v <gosu1203@gmail.com>
2023-02-17 23:14:47 +0200

    added tests

---
+++ src/test/java/tests/VpnInstructionsTest.java
@@ -0,0 +1,91 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.MainPage;
+import pages.top_menu.VpnInstructionsPage;
+
+import java.util.List;
+
+    public class VpnInstructionsTest extends BaseTest {
+        @Test
+        public void testH2TextsVpnInstructionsPage() {
+            final List<String> expectedH2Texts = List.of(
+                    "Safari Browser (Mac OS)",
+                    "Brave Browser",
+                    "Opera Browser",
+                    "Vivaldi Browser",
+                    "MS Edge (Chromium) Browser",
+                    "Microsoft Internet Explorer Browser",
+                    "Windows 10 OS",
+                    "Android OS"
+
+            );
+            final List<String> actualH2Texts = openBaseURL()
+                    .clickVPNTopMenu()
+                    .closeWindow()
+                    .switchToVpnPage()
+                    .clickInstructionsLink()
+                    .getH2Texts();
+
+            Assert.assertTrue(actualH2Texts.size() > 0);
+            Assert.assertEquals(actualH2Texts, expectedH2Texts);
+        }
+
+        @Test
+        public void testVpnLogoNavigatesToBaseURL() {
+            final String expectedURL = "https://dev.swisscows.com/en";
+            final String expectedTitle = "Your private and anonymous search engine Swisscows";
+
+            MainPage mainPage = openBaseURL();
+            final String actualURL = mainPage
+                    .clickVPNTopMenu()
+                    .closeWindow()
+                    .switchToVpnPage()
+                    .clickInstructionsLink()
+                    .clickLogo()
+                    .getCurrentURL();
+
+            final String actualTitle = mainPage.getTitle();
+
+            Assert.assertEquals(actualURL, expectedURL);
+            Assert.assertEquals(actualTitle, expectedTitle);
+        }
+
+        @Test
+        public void testTextsFontSizesVpnInstructionsPage(){
+            final List<String> expectedH1FontSizes = List.of(
+                    "24px",
+                    "24px",
+                    "24px",
+                    "24px",
+                    "24px",
+                    "24px",
+                    "24px",
+                    "24px"
+            );
+            final List<String>  actualH2FontSizes = openBaseURL()
+                    .clickVPNTopMenu()
+                    .closeWindow()
+                    .switchToVpnPage()
+                    .clickInstructionsLink()
+                    .getH2FontSizes();
+
+            Assert.assertTrue(actualH2FontSizes.size() > 0);
+            Assert.assertEquals(actualH2FontSizes, expectedH1FontSizes);
+        }
+
+        @Test
+        public void testAllBrowserIconsExist() {
+            VpnInstructionsPage vpnInstructionsPage = openBaseURL()
+                    .clickVPNTopMenu()
+                    .closeWindow()
+                    .switchToVpnPage()
+                    .clickInstructionsLink();
+
+            Assert.assertTrue(vpnInstructionsPage.isLogoIconDisplayed());
+            Assert.assertTrue(vpnInstructionsPage.allElementsDisplayed());
+        }
+    }
+
