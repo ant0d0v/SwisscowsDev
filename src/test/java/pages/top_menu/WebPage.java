3c050ff2bfc40894207525af35eddfb5b02cbe8b
Anton Udovychenko <97831314+ant0d0v@users.noreply.github.com>
2023-03-20 17:57:34 +0200

    Webtest (#92)
    * added tests for webpage

--- src/test/java/pages/top_menu/WebPage.java
+++ src/test/java/pages/top_menu/WebPage.java
@@ -1,5 +1,6 @@
 package pages.top_menu;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -30,6 +31,43 @@ public class WebPage extends TopMenuPage<WebPage> {
     private List<WebElement> allImageNewsPage;
     @FindBy(xpath = "//ul[@class='pagination']//li[3]")
     private WebElement secondPagePagination;
+    @FindBy(xpath = "//div[@class='related-searches fade in']//li//a")
+    private List<WebElement> listRelatedSearches;
+    @FindBy(xpath = "//div[@class='related-searches fade in']//li//a[1]")
+    private WebElement firstTitleRelatedSearches;
+    @FindBy(xpath = "//article[@class = 'item-web']//h2")
+    private List<WebElement> listWebResult;
+    @FindBy(xpath = "//p[@class='hint'][text()]")
+    private WebElement didYpuMeanMessage;
+    @FindBy(xpath = "//button[@class][2]")
+    private WebElement nextButtonInVideoWidget;
+    @FindBy(xpath = "//div[@class='widget widget-video']//button[@class][1]")
+    private WebElement prevButtonInVideoWidget;
+    @FindBy(xpath = "//div[last()]/article/a[1]/figure")
+    private WebElement lastImageInVideoWidget;
+    @FindBy(xpath = "//div[1]/article/a[1]/figure/img")
+    private WebElement firstImageInVideoWidget;
+    @FindBy(xpath = "//div[@class='widget widget-video']//a[@class='widget-button button']")
+    private WebElement buttonMoreVideo;
+    @FindBy(xpath = "//a[@class='widget-button button'][text()='More images']")
+    private WebElement buttonMoreImages;
+    @FindBy(xpath = "//div[@class='video-player']")
+    private WebElement videoPlayer;
+    @FindBy(xpath = "//div[@class='widget-images']//figure//img[@src]")
+    private List<WebElement>imagesInImageWidget;
+    @FindBy(xpath = "//div[@class='widget-images']")
+    private WebElement frameImageWidget;
+    @FindBy(xpath = "//div[@class='widget-images']//figure//img")
+    private List<WebElement>imagesInNewsWidget;
+
+    @FindBy(xpath = "//div[@class='widget-images']//figure//img[1]")
+    private WebElement firstImageInImageWidget;
+    @FindBy(xpath = "//div[@class='widget-images']//figure//img[1]")
+    private WebElement firstNewsInNewsWidget;
+    @FindBy(xpath = "//div[@class='widget']//p[@class='widget-title'][text()='Images for ']")
+    private WebElement titleImageWidget;
+    @FindBy(xpath = "//div[@class='widget']//p[@class='widget-title'][text()='News for ']")
+    private WebElement titleNewsWidget;
     public WebPage(WebDriver driver) {
         super(driver);
     }
@@ -76,9 +114,21 @@ public class WebPage extends TopMenuPage<WebPage> {
 
         return getText(h2Text);
     }
+    public String getTextDidYpuMeanMessage()  {
+
+        return getText(didYpuMeanMessage);
+    }
 
     public String getH2FontSize(){
         return  getFontSize(h2Text);
+
+    }
+    public String getTittleImagesWidget(){
+        return  getText(titleImageWidget);
+
+    }
+    public String getTittleNewsWidget(){
+        return  getText(titleNewsWidget);
 
     }
     public NewsPage clickRegionBrazil() {
@@ -104,6 +154,107 @@ public class WebPage extends TopMenuPage<WebPage> {
 
         return getSrcOfElements(allImageNewsPage);
     }
+    public List<String> getTextsColorsWhenHover() throws InterruptedException {
 
+        return  getHoverColorsOfElements(listRelatedSearches);
+    }
+    public List<String> getTextColors() throws InterruptedException {
+
+        return  getColorsOfElements(listRelatedSearches);
+    }
+    public List <String> getTitleInRelatedSearches()  {
+        return getTexts(listRelatedSearches);
+    }
+    public List <String> getTitleInWebResult()  {
+
+        return getTexts(listWebResult);
+    }
+    public WebPage clickFirstTitleInRelatedSearches()  {
+        clickByJavaScript(firstTitleRelatedSearches);
+        return this;
+    }
+    public void clickMoreVideoInVideoWidget()  {
+
+        click(buttonMoreVideo);
+    }
+    public ImagePage clickMoreImagesInVideoWidget()  {
+
+        click(buttonMoreImages);
+        return new ImagePage(getDriver());
+    }
+    public ImagePage clickFirstImageInImageWidget()  {
+
+        clickByJavaScript(firstImageInImageWidget);
+        return new ImagePage(getDriver());
+    }
+    public ImagePage clickFirstNewsInNewsWidget()  {
+
+        clickByJavaScript(firstNewsInNewsWidget);
+        return new ImagePage(getDriver());
+    }
+    public void clickNextButtonVideoWidget() {
+
+        clickElementUntilInvisible(nextButtonInVideoWidget);
+    }
+    public WebPage clickFirstVideoInVideoWidget() {
+
+        clickByJavaScript(firstImageInVideoWidget);
+        return  this;
+    }
+    public void clickPrevButtonVideoWidget() {
+
+        clickElementUntilInvisible(prevButtonInVideoWidget);
+    }
+    public boolean lastImageInVideoWidgetIsDisplayed() {
+        wait10ElementToBeVisible(lastImageInVideoWidget);
+        return isElementDisplayed(lastImageInVideoWidget);
+
+    }
+    public boolean firstImageInVideoWidgetIsDisplayed() {
+        wait10ElementToBeVisible(firstImageInVideoWidget);
+        return isElementDisplayed(firstImageInVideoWidget);
+
+    }
+    public VideoPage waitIUntilVisiblyVideoPlayer() {
+        wait10ElementToBeVisible(videoPlayer);
+        return new VideoPage(getDriver());
+
+    }
+    public WebPage waitForImageIsVisibleInImagesWidget(){
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        for (WebElement image : imagesInImageWidget) {
+            getWait10().until(driver -> (Boolean) js.executeScript(
+                    "return arguments[0].complete && typeof arguments[0].naturalWidth !== 'undefined' && arguments[0].naturalWidth > 0;",
+                    image));
+        }
+        return this;
+
+    }
+
+    public WebPage waitForImageIsVisibleInNewsWidget(){
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        for (WebElement image : imagesInNewsWidget) {
+            getWait10().until(driver -> (Boolean) js.executeScript(
+                    "return arguments[0].complete && typeof arguments[0].naturalWidth !== 'undefined' && arguments[0].naturalWidth > 0;",
+                    image));
+        }
+        return this;
+
+    }
+    public boolean imagesInImageWidgetIsDisplayed() {
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        for (WebElement image : imagesInImageWidget) {
+         return (Boolean) js.executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth != 'undefined' && arguments[0].naturalWidth > 0", image);
+        }
+        return imagesInNewsWidgetIsDisplayed();
+    }
+
+    public boolean imagesInNewsWidgetIsDisplayed(){
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        for (WebElement image : imagesInNewsWidget) {
+            return (Boolean) js.executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth != 'undefined' && arguments[0].naturalWidth > 0", image);
+        }
+        return imagesInNewsWidgetIsDisplayed();
+    }
 
 }
---
+++ src/test/java/tests/WebPageTest.java
@@ -0,0 +1,275 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.MainPage;
+import pages.top_menu.ImagePage;
+import pages.top_menu.NewsPage;
+import pages.top_menu.VideoPage;
+import pages.top_menu.WebPage;
+import tests.retrytest.Retry;
+
+import java.util.List;
+
+public class WebPageTest extends BaseTest {
+    @Test
+    public void testSuggestEqualsSearchCriteria_WebSearch() {
+        final String query = "ivanka";
+
+        MainPage mainPage = openBaseURL();
+        openBaseURL()
+                .inputSearchCriteriaAndEnter(query)
+                .waitUntilVisibilityWebResult()
+                .clickSearchFieldHeader();
+        mainPage
+                .waitForSuggestToBeVisible();
+
+        final List<String> actualSuggestion = mainPage.getAllElementsText();
+        final int actualSizeSuggest = mainPage.countElementsInSuggestContainer();
+
+        Assert.assertEquals(mainPage.getAllElementsText().size(), 5);
+
+        for (String searchCriteria : actualSuggestion) {
+            Assert.assertTrue(mainPage.suggestIsDisplayed());
+            Assert.assertTrue(actualSizeSuggest > 0);
+            Assert.assertTrue(searchCriteria.contains(query));
+        }
+    }
+
+    @Test
+    public void test404PageError_WebPage() {
+        WebPage webPage = new WebPage(getDriver());
+
+        final String expectedTitle404Error = "No results found for \"yquwhjsbcfkjascgfiaff%^$&\"";
+        final String expectedFontSizeTitle404Error = "40px";
+        final String actualTitle404Error = openBaseURL()
+                .inputSearchCriteriaAndEnter("yquwhjsbcfkjascgfiaff%^$&")
+                .waitUntilVisibilityErrorImage()
+                .getTitleH2Text();
+
+
+        final String actualFontSizeTitle404Error = webPage.getH2FontSize();
+
+        Assert.assertEquals(actualTitle404Error, expectedTitle404Error);
+        Assert.assertTrue(webPage.errorImageIsDisplayed());
+        Assert.assertEquals(actualFontSizeTitle404Error, expectedFontSizeTitle404Error);
+    }
+
+    @Test
+    public void testHoverTextsRelatedSearch_WebPage() throws InterruptedException {
+        WebPage webPage = new WebPage(getDriver());
+        openBaseURL()
+                .inputSearchCriteriaAndEnter("ronaldo")
+                .waitUntilVisibilityWebResult();
+
+        final List<String> oldTextsColorsWhenHover = webPage.getTextColors();
+        final List<String> newTextsColorsWhenHover = webPage.getTextsColorsWhenHover();
+
+        Assert.assertNotEquals(newTextsColorsWhenHover, oldTextsColorsWhenHover);
+    }
+
+    @Test
+    public void testRelatedSearchCriteria_WebPage() {
+        WebPage webPage = new WebPage(getDriver());
+        openBaseURL()
+                .inputSearchCriteriaAndEnter("ronaldo")
+                .waitUntilVisibilityWebResult()
+                .clickHamburgerMenu()
+                .clickRegionTopMenu()
+                .clickRegionGerman()
+                .waitForUrlContains("https://dev.swisscows.com/en/web?query=ronaldo&region=");
+
+        final String actualRegion = webPage.getCurrentURL();
+        final List<String> titleAllVideo = webPage.getTitleInRelatedSearches();
+
+        Assert.assertEquals(actualRegion, "https://dev.swisscows.com/en/web?query=ronaldo&region=de-DE");
+        for (String search : titleAllVideo) {
+            Assert.assertTrue(search.toLowerCase().contains("ronaldo"));
+        }
+    }
+
+    @Test
+    public void testClickSearchCriteriaInRelatedSearch_WebPage() {
+        WebPage webPage = new WebPage(getDriver());
+        final List<String> oldSearchResult = openBaseURL()
+                .inputSearchCriteriaAndEnter("ronaldo")
+                .waitUntilVisibilityWebResult()
+                .getTitleInWebResult();
+
+        final List<String> newSearchResult = webPage
+                .clickFirstTitleInRelatedSearches()
+                .getTitleInWebResult();
+
+        Assert.assertNotEquals(oldSearchResult, newSearchResult);
+        Assert.assertEquals(webPage.getTitleInRelatedSearches().size(), 8);
+
+    }
+
+    @Test
+    public void testSearchFieldDidYouMeanMessage_webPage() {
+
+        final String expectedResult = "[Do you want results only for appple?]";
+        final String actualResult = openBaseURL()
+                .inputSearchCriteriaAndEnter("appple")
+                .waitUntilVisibilityWebResult()
+                .getTextDidYpuMeanMessage();
+
+
+        Assert.assertNotEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void testWebResultsEqualsSearchCriteria() {
+        WebPage webPage = new WebPage(getDriver());
+        final List<String> titles = openBaseURL()
+                .inputSearchCriteriaAndEnter("ronaldo")
+                .waitUntilVisibilityWebResult()
+                .getTitleInWebResult();
+
+        final int actualSize = webPage.getTitleInWebResult().size();
+
+        Assert.assertTrue(actualSize >= 8);
+        for (String searchCriteria : titles) {
+            Assert.assertTrue(searchCriteria.toLowerCase().contains("ronaldo"));
+        }
+
+    }
+
+    @Test
+    public void testNextButtonAndPrevButtonVideoWidget_WebPage() {
+        WebPage webPage = new WebPage(getDriver());
+        openBaseURL()
+                .inputSearchCriteriaAndEnter("ronaldo video")
+                .waitUntilVisibilityWebResult()
+                .clickHamburgerMenu()
+                .clickRegionTopMenu()
+                .clickRegionGerman()
+                .waitForUrlContains("https://dev.swisscows.com/en/web?query=ronaldo+video&region=");
+        webPage
+                .clickNextButtonVideoWidget();
+        Assert.assertTrue(webPage.lastImageInVideoWidgetIsDisplayed());
+        webPage
+                .clickPrevButtonVideoWidget();
+        Assert.assertTrue(webPage.firstImageInVideoWidgetIsDisplayed());
+
+    }
+
+    @Test
+    public void testClickMoreVideoButtonInVideoWidget_WebPage() {
+        WebPage webPage = new WebPage(getDriver());
+        openBaseURL()
+                .inputSearchCriteriaAndEnter("ronaldo video")
+                .waitUntilVisibilityWebResult()
+                .clickHamburgerMenu()
+                .clickRegionTopMenu()
+                .clickRegionGerman()
+                .waitForUrlContains("https://dev.swisscows.com/en/web?query=ronaldo+video&region=");
+        webPage
+                .clickMoreVideoInVideoWidget();
+        Assert.assertTrue(getExternalPageURL().contains("https://dev.swisscows.com/en/video?query=ronaldo%20video&region=de-DE"));
+
+    }
+
+    @Test
+    public void testOpenVideoInVideoWidget_WebPage() {
+        WebPage webPage = new WebPage(getDriver());
+        final String expectedTitle = "Your private and anonymous search engine Swisscows";
+        openBaseURL()
+                .inputSearchCriteriaAndEnter("ronaldo video")
+                .waitUntilVisibilityWebResult()
+                .clickHamburgerMenu()
+                .clickRegionTopMenu()
+                .clickRegionGerman()
+                .waitForUrlContains("https://dev.swisscows.com/en/web?query=ronaldo+video&region=");
+        webPage
+                .clickFirstVideoInVideoWidget()
+                .waitIUntilVisiblyVideoPlayer();
+
+        Assert.assertTrue(webPage.getCurrentURL().contains("https://dev.swisscows.com/en/video/watch?query=ronaldo%20video&region=de-DE&id"));
+        Assert.assertEquals(getExternalPageTitle(), expectedTitle);
+
+    }
+
+    @Test
+    public void testClickMoreImageButtonInImageWidget_WebPage() {
+        WebPage webPage = new WebPage(getDriver());
+
+        final String expectedTitle = "ronaldo in Images search - Swisscows";
+        openBaseURL()
+                .inputSearchCriteriaAndEnter("ronaldo")
+                .waitUntilVisibilityWebResult()
+                .clickMoreImagesInVideoWidget()
+                .waitForImageIsVisible();
+
+        Assert.assertEquals(webPage.getCurrentURL(), "https://dev.swisscows.com/en/images?query=ronaldo");
+        Assert.assertEquals(webPage.getTitle(), expectedTitle);
+
+    }
+
+    @Test
+    public void testImagesAndTitleIsDysplaedInImageWidget_WebPage() {
+        WebPage webPage = new WebPage(getDriver());
+        final String expectedTitle = "Images for flover";
+
+        final String actualTitle = openBaseURL()
+                .inputSearchCriteriaAndEnter("flover")
+                .waitUntilVisibilityWebResult()
+                .waitForImageIsVisibleInImagesWidget()
+                .getTittleImagesWidget();
+
+
+        Assert.assertTrue(webPage.imagesInImageWidgetIsDisplayed());
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+    }
+
+    @Test
+    public void testOpenImageInTheImageWidget_WebPage() {
+        WebPage webPage = new WebPage(getDriver());
+
+        final String oldUrl = openBaseURL()
+                .inputSearchCriteriaAndEnter("ronaldo")
+                .waitUntilVisibilityWebResult()
+                .getCurrentURL();
+        webPage
+                .clickFirstImageInImageWidget()
+                .switchToExternalPage();
+
+        Assert.assertNotEquals(getExternalPageURL(), oldUrl);
+
+
+    }
+
+    @Test
+    public void testOpenNewsInTheNewsWidget_WebPage() {
+        WebPage webPage = new WebPage(getDriver());
+
+        final String oldUrl = openBaseURL()
+                .inputSearchCriteriaAndEnter("ronaldo")
+                .waitUntilVisibilityWebResult()
+                .getCurrentURL();
+        webPage
+                .clickFirstNewsInNewsWidget()
+                .switchToExternalPage();
+
+        Assert.assertNotEquals(getExternalPageURL(), oldUrl);
+
+    }
+
+    @Test
+    public void testImagesAndTitleIsDysplaedInNewsWidget_WebPage() {
+        WebPage webPage = new WebPage(getDriver());
+        final String expectedTitle = "News for ronaldo";
+
+        final String actualTitle = openBaseURL()
+                .inputSearchCriteriaAndEnter("ronaldo")
+                .waitUntilVisibilityWebResult()
+                .waitForImageIsVisibleInNewsWidget()
+                .getTittleNewsWidget();
+
+        Assert.assertTrue(webPage.imagesInNewsWidgetIsDisplayed());
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+}
