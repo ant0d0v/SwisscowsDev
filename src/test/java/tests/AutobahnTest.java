386adc994e570ad5459015fba1d6a33d85a2a473
ant0d0v <gosu1203@gmail.com>
2023-06-12 23:58:23 +0300

    refractor  autoban

--- src/test/java/tests/AutobahnTest.java
+++ src/test/java/tests/AutobahnTest.java
@@ -21,9 +21,7 @@ public class AutobahnTest extends BaseTest {
         for (int i = 0; i < 15; i++)  {
             webPage.searchAfterClear( "\"" + TestUtils.getRandomNameForBrazilBots() + " " + TestUtils.getRandomNameForBrazilBots()
                    + " " + TestUtils.getRandomNameForBrazilBots() + "\"");
-            webPage
-                    .waitUntilLoaderToBeInvisible()
-                    .waitUntilToBeVisibleTitlesInWebResult();
+            webPage.waitUntilVisibilityErrorImage();
             if (i == 11 - 1) {
                Assert.assertTrue(webPage.getTitleErrorText().contains(expectedErrorMessage));
                 break;
@@ -44,9 +42,6 @@ public class AutobahnTest extends BaseTest {
 
         for (int i = 0; i < 130; i++)  {
             webPage.searchAfterClear(TestUtils.getRandomName());
-            webPage
-                    .waitUntilLoaderToBeInvisible()
-                    .waitUntilToBeVisibleTitlesInWebResult();
             if (i == 130 - 1) {
                 Assert.assertTrue(webPage.getTitleErrorText().contains(expectedErrorMessage));
                 break;
