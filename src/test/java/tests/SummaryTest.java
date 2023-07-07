1e2c17ff504d07416ec1794dad185a1dc241ebf5
Anton Udovychenko <97831314+ant0d0v@users.noreply.github.com>
2023-07-07 12:04:30 +0300

    change tests (#141)

--- src/test/java/tests/SummaryTest.java
+++ src/test/java/tests/SummaryTest.java
@@ -1,16 +1,17 @@
 package tests;
 
 import base.BaseTest;
+import io.qase.api.annotation.QaseId;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.MainPage;
 
 
 public class SummaryTest extends BaseTest {
+    @QaseId(value = 4876)
     @Test
     public void testOpenVersionTxtSwisscowsPage() {
         MainPage mainPage = new MainPage(getDriver());
-
         final String expectedTextInSummaryPage = "3.5";
 
 
@@ -20,6 +21,7 @@ public class SummaryTest extends BaseTest {
 
         Assert.assertTrue(actualTitle404Error.contains(expectedTextInSummaryPage));
     }
+    @QaseId(value = 4883)
     @Test
     public void testOpenVersionTxtAccountPage() {
         MainPage mainPage = new MainPage(getDriver());
